446d33e974e5c25b6d3529fde63bc4817f1db5ff
Mona Lisa <1ngvar@pm.me>
2020-09-25 17:23:22 +0300

    Set up some file structure  :football:

---
+++ application/views/template_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
+
+	<title>Главная</title>
+</head>
+<body>
+<?php include 'application/views/'.$content_view; ?>
+
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+</body>
+</html>
